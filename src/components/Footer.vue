64f722e34c7335126f6a02881b2677943f91f825
wtfranco22 <francofranco_96@hotmail.com>
2023-04-28 04:31:18 -0300

    inicio de personalizado, probando componentes

---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<script setup>
+</script>
+
+<template>
+  <!-- <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" href="/">
+          <img src="../assets/center.svg" alt="Logo" width="130" height="90">
+        </a>
+        <span class="mb-3 mb-md-0 text-muted">© 2023 Complejo center</span>
+      </div>
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#twitter"></use>
+            </svg></a></li>
+        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#instagram"></use>
+            </svg></a></li>
+        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#facebook"></use>
+            </svg></a></li>
+      </ul>
+    </footer>
+  </div> -->
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+          <img src="../assets/center.svg" alt="Logo" width="130" height="90">
+        </a>
+        <span class="mb-3 mb-md-0 text-muted">© 2023 Complejo center</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-muted" href="#">
+          <i class="bi bi-twitter" style="font-size: 1rem; color: #d40c64 ;"></i>
+        </a></li>
+        <li class="ms-3"><a class="text-muted" href="#">
+          <i class="bi bi-instagram" style="font-size: 1rem; color: #d40c64;"></i>
+        </a></li>
+        <li class="ms-3"><a class="text-muted" href="#">
+          <i class="bi bi-facebook" style="font-size: 1rem; color: #d40c64;"></i>
+        </a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scope>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css");
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-md bg-light p-5">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="../assets/center.svg" alt="Logo" width="130" height="90">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="menu">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link" href="#">Turnos</a>
+          <a class="nav-link" href="#">Nosotros</a>
+          <a class="nav-link d-block d-md-none" href="#">Ingresar</a>
+          <a class="nav-link d-block d-md-none" href="#">Crear cuenta</a>
+        </div>
+      </div>
+      <div class="d-none d-md-block" >
+        <button type="button" class="btn btn-outline-dark me-2">Ingresar</button>
+        <button type="button" class="btn btn-dark">Crear cuenta</button>
+      </div>
+    </div>
+  </nav>
+</template>
+<style scoped>
+
+</style>
+
