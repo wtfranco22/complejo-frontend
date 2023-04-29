2fe93fc1d5643139287d13ce203290464372739b
wtfranco22 <francofranco_96@hotmail.com>
2023-04-29 03:53:08 -0300

    pequeña modificacion de vista de header y footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,45 +1,25 @@
 <script setup>
+var anio = new Date().getFullYear();
 </script>
 
 <template>
-  <!-- <div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <a class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" href="/">
+  <div class="container-fluid bg-dark">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center p-3">
+      <div class="col-8 d-flex align-items-center">
+        <a href="/" class="me-2">
           <img src="../assets/center.svg" alt="Logo" width="130" height="90">
         </a>
-        <span class="mb-3 mb-md-0 text-muted">© 2023 Complejo center</span>
-      </div>
-      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-              <use xlink:href="#twitter"></use>
-            </svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-              <use xlink:href="#instagram"></use>
-            </svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-              <use xlink:href="#facebook"></use>
-            </svg></a></li>
-      </ul>
-    </footer>
-  </div> -->
-  <div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <img src="../assets/center.svg" alt="Logo" width="130" height="90">
-        </a>
-        <span class="mb-3 mb-md-0 text-muted">© 2023 Complejo center</span>
+        <span class="mt-2 text-muted"><i class="bi bi-cc-circle"></i> {{anio}} Complejo center</span>
       </div>
 
-      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#">
-          <i class="bi bi-twitter" style="font-size: 1rem; color: #d40c64 ;"></i>
+      <ul class="nav mt-2 col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-muted" target="_blank" href="https://api.whatsapp.com/send?phone=2996017699">
+          <i class="bi bi-whatsapp" style="font-size: 1rem; color: #d40c64 ;"></i>
         </a></li>
-        <li class="ms-3"><a class="text-muted" href="#">
+        <li class="ms-3"><a class="text-muted" target="_blank" href="https://www.instagram.com/wtfranco22/">
           <i class="bi bi-instagram" style="font-size: 1rem; color: #d40c64;"></i>
         </a></li>
-        <li class="ms-3"><a class="text-muted" href="#">
+        <li class="ms-3"><a class="text-muted" target="_blank" href="https://www.facebook.com/wtfranco22/">
           <i class="bi bi-facebook" style="font-size: 1rem; color: #d40c64;"></i>
         </a></li>
       </ul>
@@ -48,5 +28,4 @@
 </template>
 
 <style scope>
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css");
 </style>
