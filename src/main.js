b90613e9a8f331f16a32161c8ea4b0f253aa9ada
wtfranco22 <francofranco_96@hotmail.com>
2023-05-06 15:54:20 -0300

    agregando pinia para la gestion de estados

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/routes'
+import { createPinia } from 'pinia'
 // import './assets/main.css'
 
 createApp(App)
+.use(createPinia())
 .use(router)
 .mount('#app')
